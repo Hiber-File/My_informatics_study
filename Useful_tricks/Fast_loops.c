e899206e51eee617188037110ea8259c0525fce7
Hiber-File <hiber.file.s@gmail.com>
2017-03-30 14:53:19 +0200

    Update and rename Fast_for.c to Fast_loops.c

--- Useful_tricks/Fast_loops.c
+++ Useful_tricks/Fast_loops.c
@@ -22,6 +22,11 @@ gap();
     
 for(i=1; i<=10; printf("%d", i++ ) );      /*the same result*/
 
-    
+gap();
+  
+int a = 1;
+do printf ("%d ", a*a*a); while (++a <= 10);
+  
+  
 return 0;    
 }
