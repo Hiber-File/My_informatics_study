ba46ae3614cf2780e5182fbef243b47354be777c
Hiber-File <hiber.file.s@gmail.com>
2017-04-13 21:07:00 +0200

    Update Numeral_system_base_change.c

--- Useful_tricks/Numeral_system_base_change.c
+++ Useful_tricks/Numeral_system_base_change.c
@@ -3,10 +3,10 @@
 int main()
 {
    int liczba;
-   char bufor[64];
+   char bufor[64];                                  //Maksymalna podstawa
    printf("Podaj liczbe: ");                        //Podajemy liczbe do przekonwerotwania.
    scanf("%d", &liczba);
-   printf("W systemie binarnym: %s\n", itoa(liczba,bufor,64));        //z dec na bin i zapisuje w bufor.
+   printf("W systemie binarnym: %s\n", itoa(liczba,bufor,2));         //z dec na bin i zapisuje w bufor.
    printf("W systemie oktalnym: %s\n", itoa(liczba,bufor,8));         //z dec na okt i zapisuje w bufor.
    printf("W systemie hexadecymalnym: %s\n", itoa(liczba,bufor,16));  //z dec na hex
    return 0;
