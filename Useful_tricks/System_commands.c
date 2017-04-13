74c21f78484b75bd4355b0880a65e95859144002
Hiber-File <hiber.file.s@gmail.com>
2017-04-13 15:38:52 +0200

    Create System_commands.c

---
+++ Useful_tricks/System_commands.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+
+int main(void)
+{
+    system ("dir");	        // Wyswietlenie zawartosci aktualnego katalogu
+    system("pause");
+    system("cls");          //czysci ekran
+    system ("PAUSE");       // Wstrzymuje prace programu i czeka na wcisniecie klawisza
+    system("path");         //Wyswiela dostepne sciezki
+    system("pause");
+    system ("dir *.exe");   //Wyswietlenie zawartosci aktualnego katalogu dla plików z rozszerzeniem exe
+    return 0;
+}
